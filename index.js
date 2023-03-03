47437c69585aa347a30e2934212366a60211c32a
Leticia Sassaki <leticiac.0401@gmail.com>
2023-03-03 14:04:52 -0300

    fix switch case

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
       case 'Date+Extensions.swift':
         myOutput = 'GitHubAppTests/DateExtensionsTests';
         break;
-
+  };
   core.setOutput('myOutput', myOutput);
 } catch (error) {
   core.setFailed(error.message);
