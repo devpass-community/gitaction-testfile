811a565d2fbbd5f579d0b283fccc105d2ff0420e
Leticia Kawamoto <leticiac.0401@gmail.com>
2023-03-03 11:37:29 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+
+try {
+  const myInput = core.getInput('myInput');
+  switch (myInput) {
+      case 'Date+Extensions.swift':
+        myOutput = 'GitHubAppTests/DateExtensionsTests';
+        break;
+
+  core.setOutput('myOutput', myOutput);
+} catch (error) {
+  core.setFailed(error.message);
+}
