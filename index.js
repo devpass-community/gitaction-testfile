949754d441b56388db59850a05bf934cef9791f1
Leticia Sassaki <leticiac.0401@gmail.com>
2023-03-09 14:55:34 -0300

    add new action method

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
+const https = require('https');
+const fs = require('fs');
 
-try {
-  const myInput = core.getInput('myInput');
-  switch (myInput) {
-      case 'Date+Extensions.swift':
-        myOutput = 'GitHubAppTests/DateExtensionsTests';
-        break;
-  };
-  core.setOutput('myOutput', myOutput);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+  try {
+    const fileUrl = 'https://devpass-api-bucket.s3.amazonaws.com/testes/';
+    const fileName = core.getInput('file_name');
+
+    await downloadFile(fileUrl, fileName);
+    await exec.exec('yarn test');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+async function downloadFile(fileUrl, fileName) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(fileName);
+    https.get(fileUrl, response => {
+      response.pipe(file);
+      file.on('finish', () => {
+        file.close(resolve);
+      });
+    }).on('error', error => {
+      fs.unlink(fileName);
+      reject(error);
+    });
+  });
+}
+
+run();
